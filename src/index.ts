3697d0fbf265be13566e2e9cc37d896257b7328e
LNPappas <LPappas@gmail.com>
2020-08-14 14:31:05 -0700

    added logger, added update user to index & remote

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,25 @@
 import express, { Request, Response, NextFunction } from 'express'
 import { auth0GetUserServiceToken } from './remote/auth0/get-user-service-token'
+import { auth0UpdatePassword } from './remote/auth0/patch-password'
+import { logger } from './util/loggers';
 
 const app = express()
 
 app.use(express.json())
 
+// const basePath = process.env['AC_BASE_PATH'] || ''
+
+app.patch('/updatePassword', (req:Request, res:Response, next:NextFunction) => {
+    let { userId, password } = req.body;
+    try {
+        let update = auth0UpdatePassword(userId, password);
+        res.json(update);
+    } catch (error) {
+        logger.error(error);
+    }
+})
+
 app.listen(2006, () =>{
     auth0GetUserServiceToken()
-    console.log('Server has started!')
+    logger.info('Server has started!')
 } )
--- src/util/loggers.ts
+++ src/util/loggers.ts
@@ -1,2 +1,20 @@
-// added loggers
+import { configure, getLogger } from 'log4js'
 
+configure({
+    appenders: {
+        console: { type: 'stdout', layout: { type: 'coloured' } },
+        errorFile: { type: 'file', filename: 'logs/errors.log' }
+    },
+    categories: {
+        default: { appenders: ['console'], level: 'ALL' },
+        error: { appenders: ['errorFile'], level: 'WARN' }
+    }
+})
+
+export const logger = getLogger()
+export const errorLogger = getLogger('error')
+
+//logger.info()
+//logger.debug()
+//logger.error()
+//logger.fatal()
