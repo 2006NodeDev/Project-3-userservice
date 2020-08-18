3e063f34caf180276bffedebc8f040fa71463efc
ceciliapichardo <cecmaria23@gmail.com>
2020-08-18 09:20:37 -0700

    login endpoint

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,31 @@ import express, { Request, Response, NextFunction } from 'express'
 import { auth0GetUserServiceToken } from './remote/auth0/get-user-service-token'
 import { auth0UpdatePassword } from './remote/auth0/patch-password'
 import { logger } from './util/loggers';
+import { auth0Login } from './remote/auth0/login';
 
 const app = express()
 
 app.use(express.json())
 
 // const basePath = process.env['AC_BASE_PATH'] || ''
+
+app.post('/login', async (req:Request, res:Response, next:NextFunction) => {
+    let { username } = req.body
+    let { password } = req.body
+
+    if (!username || !password) {
+        logger.error('Bad Credentials')
+    }
+    else {
+        try {
+            let userToken = await auth0Login(username, password)
+            res.header('Authorization', `Bearer ${userToken}`)
+            res.json(userToken)
+        } catch (e) {
+            logger.error(e)
+        }
+    }
+})
 
 app.patch('/updatePassword', (req:Request, res:Response, next:NextFunction) => {
     let { userId, password } = req.body;
---
+++ src/remote/auth0/login.ts
@@ -0,0 +1,33 @@
+import { logger, errorLogger } from "../../util/loggers";
+import { auth0BaseClient } from ".";
+require('dotenv').config()
+/*
+ * This function gets the Auth0 User Token at login.
+ * The request body contains the specific properties of
+ * our Auth0 API. The response returns an access
+ * token for a user in Auth0.
+ * Env variables should be stored in a .env file inside 
+ * of the project folder.
+ */
+export async function auth0Login(username:string, password:string) {
+    try {
+        let body = {
+            client_id: process.env['AUTH0_CLIENT_ID'],
+            client_secret: process.env['AUTH0_CLIENT_SECRET'],
+            audience: 'http://companion.revature.net',
+            grant_type: 'password',
+            username,
+            password
+        }
+        let res = await auth0BaseClient.post('/oauth/token', body)
+        logger.debug(res.data)
+        return res.data.access_token
+    } catch(e) {
+        logger.debug(e)
+        errorLogger.error(e)
+        logger.info(e.status)
+        let error:any = new Error(e.message) //could be custom error
+        error.status = e.status
+        throw error
+    }
+}
