05ff44613037d38952eb665d717528ded331784e
Laura Shears <52978933+shearsl@users.noreply.github.com>
2020-08-19 04:18:58 -0400

    manually incorporated a few changes from github

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 import express from 'express'
+import { associateRouter } from './routers/associate-router';
+//import { batchRouter } from './routers/batch-router';
+import { corsFilter } from './middleware/cors-filter';
 
 const app = express()
 
 app.use(express.json())
+app.use(corsFilter)
+
+//app.use('/batches', batchRouter);
+app.use('/associates', associateRouter);
 
 app.listen(2006, () =>{
     console.log('Server has started!')
---
+++ src/routers/associate-router.ts
@@ -0,0 +1,101 @@
+import express, { Request, Response, NextFunction } from 'express'
+import { getAllAssociates } from '../remote/associate/get-all-associates'
+import { getCurrentBatches } from '../remote/batch/get-current-batches'
+import { getAssocBySkill } from '../remote/associateFilter/get-associates-filter-skill'
+import { getAssociatesWithYear } from '../remote/associateFilter/get-associate-filter-year'
+import { getAssociatesByTrainer } from '../remote/associate/get-associate-by-trainer'
+import { getSkillsList } from '../remote/batch/get-skills-list'
+
+export let associateRouter = express.Router()
+export let batchRouter = express.Router()
+
+// auth middleware goes here
+// associatesRouter.use(authenticationMiddleware);
+
+// associateRouter.get('/batches/:batchId', async (req:Request, res:Response, next:NextFunction) => {
+//     let {batchId} = req.params;
+//     try{
+//         let user = await getAssociatesByBatchId(batchId)
+//         res.json(user)
+//     } catch (e){
+//         next(e)
+//     }
+// })
+
+associateRouter.get('/', async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        let user = await getAllAssociates()
+        res.json(user)
+        console.log("return length = " + user.length)
+        console.log(user[0].email)
+
+    } catch (e) {
+        next(e)
+    }
+})
+
+
+
+
+associateRouter.get('/currentBatches', async (req: Request, res: Response, next: NextFunction) => {
+    console.log("we hit the batch router!")
+    try {
+        let batch = await getCurrentBatches()
+        res.json(batch)
+
+    } catch (e) {
+        console.log(e)
+        next(e)
+    }
+})
+
+
+
+associateRouter.get('/skill/:skillname', async (req: Request, res: Response, next: NextFunction) => {
+    let  skillName = req.params.skillname
+    try {
+        let assocBySkill = await getAssocBySkill(skillName)
+        res.json(assocBySkill)
+    } catch (e) {
+        console.log(e)
+        next(e)
+    }
+})
+
+associateRouter.get('/year/:yearValue', async (req: Request, res: Response, next: NextFunction) => {
+    let  year = req.params.yearValue
+    try {
+        let assocByYear = await getAssociatesWithYear(year)
+        console.log(assocByYear)
+        res.json(assocByYear)
+    } catch (e) {
+        console.log(e)
+        next(e)
+    }
+})
+
+//getBatchIdsByTrainer
+//As a Trainer, I should be able to view the profiles of all 
+//Associates across all my batches so that I can best accommodate
+//the preferences of my Associates. 
+// get associates by batch id
+associateRouter.get('/:trainerEmail', async (req: Request, res: Response, next: NextFunction) => {
+    let { trainerEmail } = req.params;
+    try {
+        let batches = await getAssociatesByTrainer(trainerEmail)
+       
+        res.json(batches)
+    } catch (e) {
+        next(e)
+    }
+})
+
+//gets the list of skills being taught by currently active batches
+associateRouter.get('/batch/skills', async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        let user = await getSkillsList()
+        res.json(user)
+    } catch (e) {
+        next(e)
+    }
+})
