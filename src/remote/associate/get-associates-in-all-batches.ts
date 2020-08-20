6ed5ac6bd778b73bf9c95b727b2c8414ec6e155f
Reznitchenko <inna.reznitchenko@accenture.com>
2020-08-20 16:43:16 -0400

    deleted an unnecessary file and deleted some comments

--- src/remote/associate/get-associates-in-all-batches.ts
+++ src/remote/associate/get-associates-in-all-batches.ts
@@ -5,18 +5,9 @@ import { getAssociatesByBatchId } from "./get-associates-by-batch-id"
 export const getAssocInAllBatches = async (batchList:Batch[])=>{
     let getAssocInBatch: Associate[] = []
 
-    // for (var i in batchList){
-    //     for(var j in batchList[i].associateAssignments)
-    //     getAssocInBatch.push(batchList[i].associateAssignments[j].associate)
-    // }
-
     for (var i in batchList){
-        // for(var j in batchList[i].associateAssignments)
-        // associates = await getAssociatesByBatchId(batchList[i])
         getAssocInBatch = getAssocInBatch.concat(await getAssociatesByBatchId(batchList[i].batchId))
     }
-
-    // console.log(assocArrayinSkillBatch)
     return getAssocInBatch
 
 }
