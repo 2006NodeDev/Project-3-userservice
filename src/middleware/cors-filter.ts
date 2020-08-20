7d0b71b38bc4f5f8cf28e5b60867b6bfc2815d5e
faithOnly <firdevssyildirimm@gmail.com>
2020-08-20 13:34:28 -0500

    corsfilter for authorization

--- src/middleware/cors-filter.ts
+++ src/middleware/cors-filter.ts
@@ -2,17 +2,19 @@ import { Request, Response, NextFunction } from "express";
 
 
 export function corsFilter(req:Request, res:Response, next:NextFunction){
-//we always have to set Access-Control-Allow-Origin header on every request
-    res.header('Access-Control-Allow-Origin',  `${req.headers.origin}`) // this is bad, don't do this when your app is deployed
-    res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept')
+    //we always, on every request options or not, have to set Access-Control-Allow-Origin header
+    res.header('Access-Control-Allow-Origin', `${req.headers.origin}`)//this is a dirty hack, its really bad, don't do it when you app is deployed or I will be very disappointed in you
+    res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization')
+    res.header('Access-Control-Expose-Headers', 'Authorization')//if we don't d othis, the website cannot get access to the token
     res.header('Access-Control-Allow-Credentials', 'true')
     res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE')
 
-        //purpose of option is to figure out  what kind of request allowed to the server
-        //we specifiy the kinds of request using the headers
+    //the purpose of an options request is to figure out what kind of requests are allowed to made to the server
+    //we specify the kinds of requests using the headers of the response to the options request
     if(req.method === 'OPTIONS'){
-        res.sendStatus(200) // will send back the options for pre flight requests
-    }else {
-        next()  // allow the real request to go to the endpoint
+        res.sendStatus(200)// will send back the options for pre flight requests
+    } else {
+        next()//allow the real request to go to the endpoint
     }
+
 }
